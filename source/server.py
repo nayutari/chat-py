8145cbccb245d1ef9daa96d31af8e2260858f019
nayuta <nayuta.solution@gmail.com>
2021-09-29 23:58:27 +0900

    複数クライアントからデータ受信

--- source/server.py
+++ source/server.py
@@ -1,4 +1,6 @@
 import socket
+# スレッドライブラリ取り込み
+import threading
 
 IPADDR = "127.0.0.1"
 PORT = 49152
@@ -7,10 +9,17 @@ sock_sv = socket.socket(socket.AF_INET)
 sock_sv.bind((IPADDR, PORT))
 sock_sv.listen()
 
-# クライアントの接続受付
-sock_cl, addr = sock_sv.accept()
+# データ受信ループ関数
+def recv_client(sock, addr):
+    while True:
+        data = sock.recv(1024)
+        print(data.decode("utf-8"))
 
+# クライアント接続ループ
 while True:
-    # データ受信
-    data = sock_cl.recv(1024)
-    print(data.decode("utf-8"))
+    # クライアントの接続受付
+    sock_cl, addr = sock_sv.accept()
+    # スレッドクラスのインスタンス化
+    thread = threading.Thread(target=recv_client, args=(sock_cl, addr))
+    # スレッド処理開始
+    thread.start()
