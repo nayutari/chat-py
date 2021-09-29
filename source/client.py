c7b7f7a324396cf10569f8f8c377d3df28ead269
Nayuta <nayuta.solution@gmail.com>
2021-09-29 17:19:28 +0900

    1プロセスのみ複数回の受信対応

--- source/client.py
+++ source/client.py
@@ -1,14 +1,14 @@
-# ソケットライブラリ取り込み
 import socket
 
-# サーバーIPとポート番号
 IPADDR = "127.0.0.1"
 PORT = 49152
 
-# ソケット作成
 sock = socket.socket(socket.AF_INET)
-# サーバーへ接続
 sock.connect((IPADDR, PORT))
 
-# byte 形式でデータ送信
-sock.send("hello".encode("utf-8"))
+# 送信無限ループ
+while True:
+    # 任意の文字を入力
+    data = input("> ")
+    # サーバーに送信
+    sock.send(data.encode("utf-8"))
